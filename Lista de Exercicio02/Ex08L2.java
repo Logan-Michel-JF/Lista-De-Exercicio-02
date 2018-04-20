03bd4484ea6e221268efaa0775f2d564c4d49979
LoganMichel <loganmichel1717@gmail.com>
2018-04-20 09:18:18 -0300

    alteração de texto 02 EXEMPLO

--- Lista de Exercicio02/Ex08L2.java
+++ Lista de Exercicio02/Ex08L2.java
@@ -14,8 +14,8 @@ public class Ex08L2{
                             "\nBolos           Bolo Nega Maluca               R$ 19,33" +
                             "\nDoces           Bomba de Creme                 R$ 17,71" +
                             "\nDoces           Bomba de Morango               R$ 4,82" +
-                            "\nSanduíches      Filé-mignon com                R$ 21,16
-                                               Fritas e Cheddar"+
+                            "\nSanduíches      Filé-mignon com                R$ 21,16"+
+                                              "Fritas e Cheddar"
                             
                         );
 
